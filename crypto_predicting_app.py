5a24365ffe85df83643f50b6ad3ffe5935b529ad
hoongyuan <62897998+hoongyuan@users.noreply.github.com>
2023-09-11 22:55:43 +0800

    Update crypto_predicting_app.py

--- crypto_predicting_app.py
+++ crypto_predicting_app.py
@@ -8,11 +8,11 @@ import pickle
 # import matplotlib
 
 # #for modeling
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import MinMaxScaler
+# from keras.models import Sequential
+# from keras.layers import LSTM, Dense
+# from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 #for saving model
 import pickle
