972b36ae88d7b98680265166c7ab2890ec687f9c
hoongyuan <62897998+hoongyuan@users.noreply.github.com>
2023-09-15 21:22:20 +0800

    Update crypto_predicting_app.py

--- crypto_predicting_app.py
+++ crypto_predicting_app.py
@@ -198,7 +198,7 @@ def make_prediction(model,input):
     prediction_scaled = model.predict(input)
 
     # Inverse transform the scaled predictions using the scaler
-    prediction = target_scaler.inverse_transform(prediction_scaled)
+    prediction = target_scaler.inverse_transform(prediction_scaled.reshape(-1, 1))
 
     return prediction
 
