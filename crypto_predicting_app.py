458a33dd615204e02e3e913dc684ba38934a694a
hoongyuan <62897998+hoongyuan@users.noreply.github.com>
2023-09-18 21:10:25 +0800

    Update crypto_predicting_app.py

--- crypto_predicting_app.py
+++ crypto_predicting_app.py
@@ -168,7 +168,7 @@ def show_dashboard(data):
     # date_difference_str = f"{days} days, {hours} hours"
 
     # Display dataset start and end timestamp along with the date difference
-    st.write(f"**Dataset Period:** {time_start} - {time_end} ({date_difference_str})")
+    # st.write(f"**Dataset Period:** {time_start} - {time_end} ({date_difference_str})")
 
     # Show total data rows
     st.write("**Total Rows:** ", len(df))
